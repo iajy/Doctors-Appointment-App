1c810c72821ccb7b8c5d2c470f4bf17034eb0372
Ajay Binu <ajaybinuajay2002@gmail.com>
2025-07-12 19:18:47 +0530

    calender length changed

--- Doctors-Appointment-App/src/Components/MyBigCalendar.jsx
+++ Doctors-Appointment-App/src/Components/MyBigCalendar.jsx
@@ -128,7 +128,7 @@ function MyBigCalendar() {
         onView={(view) => setDefaultView(view)}
         date={currentDate}
         onNavigate={setCurrentDate}
-        style={{ height: "70vh" }}
+        style={{ height: "65vh" }}
         onSelectSlot={(slotInfo) => {
           const clickedDate = slotInfo.start;
           if (clickedDate < new Date()) {
